be28c09bf4899cef1ad42e881d9bff9a50aa33fe
seo05 <86290105+seo05@users.noreply.github.com>
2021-06-22 12:06:38 +0300

    Create task2.ps1

---
+++ task2.ps1
@@ -0,0 +1,44 @@
+param (
+    $Pathto=$args[0]
+)
+$outcsv="C:\scripts\acc\accounts_new.csv"
+$data=@(Get-Content -Path $Pathto)
+$addPart="@abc.com"
+foreach ($item in $data) {
+    if (($item.Split(",") | Select-Object -Index 0) -eq "id") { # for header
+        $item >> $outcsv
+    }
+    else {   
+        $id=$item.Split(",") | Select-Object -Index 0
+        $location_id=$item.Split(",") | Select-Object -Index 1
+        $oldname=$item.Split(",") | Select-Object -Index 2
+        $name=((Get-Culture).TextInfo).ToTitleCase($oldname)
+        if ($item -cmatch '"'){ #for "Manager, Commanding Officer" etc.
+            $title=(($item.Split(",") | Select-Object -Index 3,4) -join ",")
+            $department=$item.Split(",") | Select-Object -Index 6
+        }
+        else {
+            $title=$item.Split(",") | Select-Object -Index 3
+            $department=$item.Split(",") | Select-Object -Index 5
+        }
+        if ($item -cmatch '@'){ #for e.feeney@foobar.org etc.
+            $email=$item.Split(",") | Select-Object -Index 4
+        }
+        else {
+            if ($oldname -cmatch '-' ){ #for marilyn baker-"V"enturini
+                $lowercase=(($oldname.Split(" ") | ForEach-Object { $_ -replace $_[0], $([char]::ToLower($_[0]))}) -join " " )
+                $lowercase1=(($lowercase.Split("-") | ForEach-Object { $_ -replace $_[0], $([char]::ToLower($_[0]))}) -join "-" )
+                $firstLetter=$lowercase1.Split(" ") | Select-Object -Index 0 | ForEach-Object { $_ -replace $_, $_[0]}
+                $surname=$lowercase1.Split(" ") | Select-Object -Index 1
+                $email=-join($firstLetter,$surname,$location_id,$addPart)
+            }
+            else {
+                $lowercase=(($oldname.Split(" ") | ForEach-Object { $_ -replace $_[0], $([char]::ToLower($_[0]))}) -join " " )
+                $firstLetter=$lowercase.Split(" ") | Select-Object -Index 0 | ForEach-Object { $_ -replace $_, $_[0]}
+                $surname=$lowercase.Split(" ") | Select-Object -Index 1
+                $email=-join($firstLetter,$surname,$location_id,$addPart)
+            }
+        }
+    ($id,$location_id,$name,$title,$email,$department) -join "," >> $outcsv
+    }
+}
